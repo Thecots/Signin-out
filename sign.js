c086a37b5af7720be27f270b48a7e129f5633164
Thecots <thecots22@gmail.com>
2021-11-17 23:19:53 +0100

    js - donde

--- sign.js
+++ sign.js
@@ -1,24 +1,19 @@
-const loadBar = document.querySelector('.loadBar'); 
-const spanError = document.querySelector('#sessionError');
-const spanErrorUsername = document.querySelector('#sessionErrorUsername');
-const usernameInput = document.getElementById("username");
-const passwordInput = document.getElementById("password");
-const confirmInput = document.getElementById("confirm");
+const loadBar = document.querySelector('.loadBar'), spanError = document.querySelector('#sessionError'),spanErrorUsername = document.querySelector('#sessionErrorUsername'), usernameInput = document.getElementById("username"),passwordInput = document.getElementById("password"), confirmInput = document.getElementById("confirm"), AlertIcon = '<svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg>'
 
-const AlertIcon = '<svg aria-hidden="true" class="stUf5b LxE1Id" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg>'
+/* ----- windows.location.href = singup ----- */
+const createAccount = () =>{ 
+    window.location.href = './signup.html';
+};
+/* ----- windows.location.href = signin ----- */
+const userHaveAccount = () =>{ 
+    window.location.href = './signin.html';
+};
 
-/* Enciender/apagar circulo de buscando -> in */
+/* ---- toggle on/off load bar ---- */
 function setloadbar(e){
-    if(e == true){
-        loadBar.style.display = "flex";
-    }else{
-        loadBar.style.display = "none";
-    }
+    loadBar.style.display = e == true ? "flex" : "none"
 }
-
-setloadbar(false);
-
-/* Aparece/desaparece texto de error -> in */
+/* ---- toggle on/off error text ---- */
 function error(type,e,t){
     if(type == "password" && e == true){
         spanError.innerHTML = AlertIcon+t;
@@ -26,7 +21,6 @@ function error(type,e,t){
     }else if(e == false){
         spanError.style.display = "none";
     }
-
     if(type == "username" && e == true){
         spanErrorUsername.innerHTML = AlertIcon+t;
         spanErrorUsername.style.display = "flex";
@@ -34,9 +28,30 @@ function error(type,e,t){
         spanErrorUsername.style.display = "none";
     }
 }
+/* ---- toggle on/off show passowrd ----*/
+const showPasword = () =>{
+    if (document.querySelector('#showPassword').checked) {
+        document.querySelector('#passwd1').type = 'text';
+        document.querySelector('#passwd2').type = 'text';
+    }else{
+        document.querySelector('#passwd1').type = 'password';
+        document.querySelector('#passwd2').type = 'password';
+    }
+}
 
-/* On click  "Crear cuenta" redirect to signiup*/
-function iniciarSession(){
+/* ---- remove all errors text ---- */
+function restartErrors(){
+    usernameInput.classList.remove('error');
+    passwordInput.classList.remove('error');
+    try {
+        confirmInput.classList.remove('error');
+    } catch (error) {}
+    error("username",false);
+    error("password",false);
+}
+
+/* ----- sign in -----*/
+const iniciarSession = () =>{
     restartErrors();
     let elements = document.getElementById("formIn").elements;
     let u = elements.item(0).value, p = elements.item(1).value;
@@ -52,30 +67,11 @@ function iniciarSession(){
         return 0;
     }
     var obj = {username: u, password:p};
-    let tojson = JSON.stringify(obj);
+    
 }
 
-/* On click  "Crear cuenta" redirect to signiup*/
-function createAccount(){ 
-    window.location.href = './signup.html';
-};
-
-/* On click  "Ya tengo cuenta" redirect to signiup*/
-function userHaveAccount(){ 
-    window.location.href = './signin.html';
-};
-
-/* Remove all errors */
-function restartErrors(){
-    usernameInput.classList.remove('error');
-    passwordInput.classList.remove('error');
-    confirmInput.classList.remove('error');
-    error("username",false);
-    error("password",false);
-}
-
-/* On click Create Account */
-function crearCuenta(){
+/* ----- sing up ----- */
+const crearCuenta = () =>{
     restartErrors();
     let elements = document.getElementById("formIn").elements;
     let u = elements.item(0).value, p = elements.item(1).value, c = elements.item(2).value;
@@ -102,16 +98,4 @@ function crearCuenta(){
             return 0;
         }
     var obj = {username: u, password:p};
-    let tojson = JSON.stringify(obj);
-}
-
-/* On click "Mostrar contrasela" inputs type = password transforms in text || password*/
-function showPasword(){
-    if (document.getElementById('showPassword').checked) {
-        document.getElementById('passwd1').type = 'text';
-        document.getElementById('passwd2').type = 'text';
-    }else{
-        document.getElementById('passwd1').type = 'password';
-        document.getElementById('passwd2').type = 'password';
-    }
 }
